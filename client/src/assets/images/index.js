a5a9e1b848b4a8c07c7e3a02e2135fcddc7f82c9
Thinakaran <thinakaranmanohar@yahoo.com>
2025-05-07 23:38:08 +0530

    Whoo

--- client/src/assets/images/index.js
+++ client/src/assets/images/index.js
@@ -2,6 +2,8 @@ import Logo from "./public/logo/Description--Streamline-Plump.svg"
 import HeroUser from "./public/hero/herouser.jpg"
 import CatGIF from "./public/hero/CatWorkingGIF.gif"
 
+import Pattern from "./public/hero/4-point-stars.svg"
+
 export default {
-    Logo, HeroUser, CatGIF,
+    Logo, HeroUser, CatGIF, Pattern,
 }
--- client/src/pages/public/Home.jsx
+++ client/src/pages/public/Home.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import "./../../index.css"
 import images from '../../assets/images'
 import { HorizontalReviewCard } from '../../components'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <div className='bg-bggrey w-full h-full px-6 ' >
+        <div className='bg-bggrey w-full h-full px-6 ' style={{ backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cg fill='%23121514' fill-opacity='0.1'%3E%3Cpolygon fill-rule='evenodd' points='8 4 12 6 8 8 6 12 4 8 0 6 4 4 6 0 8 4'/%3E%3C/g%3E%3C/svg%3E")` }} >
             <div>
                 <div>
                     <div className=' px-6 h-screen flex items-center py-12 ' >
@@ -14,6 +15,12 @@ const Home = () => {
                                 <span>What They Say Can Build Empires</span>
                                 <span> — Let It Echo Loud</span>
                             </h1>
+                            <div className='' >
+                                <div className=" flex space-x-3 mt-16 " >
+                                    <Link className='bg-bgpink text-textdark px-6 py-3 rounded-full font-para_inter text-base ' to="/docs/get-started" >Get started</Link>
+                                    <Link className='bg-bgdark text-white px-6 py-3 rounded-full font-para_inter text-base ' to="/register" >Register</Link>
+                                </div>
+                            </div>
                         </div>
                         <div className='w-2/5 h-full flex  flex-col justify-between ' >
                             <div className='space-y-6' >
@@ -31,8 +38,8 @@ const Home = () => {
                             </div>
                             <div className='space-y-2 relative' >
                                 <div className='flex justify-end ' >
-                                    <div className='rounded-3xl p-2 bg-[#00000010] w-1/3  ' >
-                                        <img src={images.CatGIF} className='h-28 w-full object-cover object-center rounded-2xl' alt="" srcset="" />
+                                    <div className='rounded-3xl p-2 shadow-sm bg-[#00000010] w-1/3  ' >
+                                        <img src={images.CatGIF} className='h-28 w-full object-cover object-center rounded-2xl ' alt="" srcset="" />
                                     </div>
                                 </div>
                                 <div className="bg-[#00000010] w-fit p-1.5 rounded-xl text-lg left-6 top-20 -rotate-3 absolute" >
